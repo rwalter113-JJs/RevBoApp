f6fa0f627c64f6ee49e7eeef6f26afb3eb5131dd
Rob Walter <rob@revbo.ai>
2026-04-08 08:22:41 -0400

    Initial Commit

---
+++ RevBoApp/RevBoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RevBoAppApp.swift
+//  RevBoApp
+//
+//  Created by Robert Walter on 4/8/26.
+//
+
+import SwiftUI
+
+@main
+struct RevBoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
